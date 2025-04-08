23332de243a4d7c0989c9323aba130c34bc5b80c
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-04-08 16:57:49 +0900

    no message

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Sales(props) {
     return (
         <>
-            <main>
+                <div>
                 <h1>매출현황</h1>
-            </main>
+                </div>
         </>
     );
 }
