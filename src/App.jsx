b8c503968c10ff549f109736f5d9341d89d54c46
DESKTOP-29VI7GO\user2 <haracedaily>
2025-04-10 13:01:26 +0900

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Header from "./layout/Header.jsx";
 import Side from "./layout/Side.jsx";
 import Body from "./layout/Body.jsx";
-import Footer from "./layout/Footer.jsx";
-import {BrowserRouter} from "react-router-dom";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function App() {
