480c5b511bf6724836737b7575991411d9c8137d
DESKTOP-29VI7GO\user2 <haracedaily>
2025-04-11 17:36:08 +0900

    no message

--- src/js/sideMenu.js
+++ src/js/sideMenu.js
@@ -1,8 +1,8 @@
 const sideMenu = [
-    {value:'대시보드',link:'/',state:'isStatus'},
-    {value:'예약관리',link:'/reservation',state:'isReserved'},
-    {value:'게시글관리',link:'/contact',state:'isNotice'},
-    {value:'사원관리',link:'/employee',state:''},
+    {value:'대시보드',link:'/',state:'dashboard'},
+    {value:'예약관리',link:'/reservation',state:'reservation'},
+    {value:'게시글관리',link:'/contact',state:'contact'},
+    {value:'사원관리',link:'/employee',state:'employee'},
 ];
 
 export default sideMenu;
