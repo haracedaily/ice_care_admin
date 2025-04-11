f519a4f4c19ae40eb231789b42a4402bad75b858
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-04-11 11:39:45 +0900

    no message

---
+++ src/js/sideMenu.js
@@ -0,0 +1,13 @@
+const sideMenu = [
+    {label:'대시보드',link:'/'},
+    {label:'예약등록',link:''},
+    {label:'예약수정',link:''},
+    {label:'예약조회',link:''},
+    {label:'게시글등록',link:''},
+    {label:'게시글수정',link:''},
+    {label:'게시글조회',link:''},
+    {label:'메뉴관리',link:''},
+    {label:'사원관리',link:''},
+];
+
+export default sideMenu;
