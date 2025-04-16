7e3fd204a888519aaa23f244d2fe57504d6c32b2
DESKTOP-29VI7GO\user <ssssssy0@naver.com>
2025-04-16 18:12:08 +0900

    no message

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,38 +1,66 @@
 import React, { useState, useEffect } from 'react';
-// import { Layout, Button, Modal, Tabs, Card, Flex } from 'antd';
-// import ReservationTable from '../components/ReservationTable';
-// import ReservationForm from '../components/ReservationForm';
-// import MiniDashboard from '../components/ResDashboard.jsx';
-// import SearchFilters from '../components/ResSearchFilters.jsx';
-// import { supabase } from '../services/supabase';
-// import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
-import { Breadcrumb } from 'antd';
+import {Breadcrumb, Layout} from 'antd';
+import ResDashboard from "../components/ResDashboard.jsx";
+import {supabase} from "../js/supabase.js";
 
 
 
-function Reservation() {
-    return (
-        <div>
-            <div>
-                <Breadcrumb
-                    separator=">"
-                    items={[
-                        {
-                            title: 'Home',
-                        },
-                        {
-                            title: '예약관리',
-                            // onClick:(e)=>{e.preventDefault();reservationNavi("/reservation");},
-                            href: '',
-                        },
-                    ]}
-                />
-            </div>
-            <div>
-            </div>
-        </div>
+const {Content} = Layout;
+
+
+const Reservation = () =>{
+    // const [reservations, setReservations] = useState([]);
+    // const [filteredReservations, setFilteredReservations] = useState([]);
+    // const [isModalOpen, setIsModalOpen] = useState(false);
+    // const [editingReservation, setEditingReservation] = useState(null);
+    const [filterType, setFilterType] = useState('all');
+    // const [filters, setFilters] = useState({
+    //     name: '',
+    //     tel: '',
+    //     email: '',
+    //     addr: '',
+    //     state: null,
+    // });
+    // const [dateRange, setDateRange] = useState([
+    //     dayjs().subtract(1, 'month').startOf('day'), // 한 달 전
+    //     dayjs().endOf('day'), // 오늘
+    // ]);
+
+    const fetchReservations = async () => {
+        let query = supabase.from('ice_res').select('*');
+
+        if(filterType === 'cleaning') {
+            query = query.eq('service', '청소');
+        }else if(filterType === 'repair') {
+            query = query.eq('service', '수리');
+        }
+    }
+
+
+
+
+    return(
+        <Layout>
+            <Breadcrumb
+                separator=">"
+                items={[
+                    {
+                        title: 'Home',
+                    },
+                    {
+                        title: '예약관리',
+                        // onClick:(e)=>{e.preventDefault();reservationNavi("/reservation");},
+                        href: '',
+                    },
+                ]}
+            />
+            <Content>
+
+            </Content>
+
+        </Layout>
     );
-}
+};
 
 export default Reservation;
