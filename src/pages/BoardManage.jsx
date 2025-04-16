994b80cbedf5c73f1fd19a781dca2872ddb1ce9e
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-04-16 17:55:19 +0900

    no message

--- src/pages/BoardManage.jsx
+++ src/pages/BoardManage.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
-import {Breadcrumb} from "antd";
+import {Breadcrumb, Button, Input, Select} from "antd";
+import {debounce} from "@mui/material";
 
-function BoardManage(props) {
+const BoardManage = () => {
+    const [searchText, setSearchText] = React.useState('');
+    const [appliedSearchText, setAppliedSearchText] = React.useState('');
+    const [filterCategory, setFilterCagegory] = React.useState('all');
+
+
+    const categories = [
+        {id: 'all', name: '전체'},
+        {id: 2, name: '공지사항'},
+        {id: 3, name: 'FAQ'},
+    ]
+
+    const debouncedSearchText = debounce(() => {
+        setSearchText(value);
+    }, 300);
+
+    // 검색조회
+    const handleSearch = () => {
+        setAppliedSearchText(searchText);
+    }
+
+    // 검색어 초기화
+    const handleReset = () => {
+        setSearchText('');
+        setAppliedSearchText('');
+    }
+
     return (
         <div className="content">
             <div className="header">
@@ -9,17 +36,34 @@ function BoardManage(props) {
                 <Breadcrumb
                     separator=">"
                     items={[
-                        {
-                            title: 'Home',
-                        },
-                        {
-                            title: '게시판관리',
-                            onClick:(e)=>{e.preventDefault(); reservationNavi("/contact");},
-                            href: '',
-                        },
-
+                        {title: 'Home',},
+                        {title: '게시판관리', href: '',},
                     ]}
                 />
+            </div>
+
+            <div style={{display: 'flex', gap: '10px'}}>
+                <Input placeholder="제목 또는 작성자 검색"
+                       value={searchText}
+                       onChange={(e) => debouncedSearchText(e.target.value)}
+                       onPressEnter={handleSearch}
+                       style={{width: '300px'}}
+                />
+                <Select placeholder="카테고리 선택"
+                        defaultValue="all"
+                        value={filterCategory}
+                        onChange{(value) => setFilterCagegory(value)}
+                        style={{width: '150px'}}
+                        allowClear={false}
+                >
+                    {categories.map((category) => (
+                        <Option key={category.id} value={category.id}>
+                            {category.name}
+                        </Option>
+                    ))}
+                </Select>
+                <Button type="primary" onClick={handleSearch}>조회</Button>
+                <Button onClick={handleReset}>초기화</Button>
             </div>
         </div>
     );
