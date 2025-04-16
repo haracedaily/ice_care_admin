c07c580e5d14baba7d6273f26fbf21b24ad46896
DESKTOP-29VI7GO\user2 <haracedaily>
2025-04-16 18:14:18 +0900

    no message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -117,54 +117,22 @@ function Home(props) {
         }
     }
 
-    const COLORS = ['#00ff00', '#ffff00', '#ff6200', '#ff0000'];
+    const COLORS = ['#93ffef', '#e7ff9f', '#ff9fe9'];
 
 
-    const renderActiveShape = (props) => {
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index,value }) => {
         const RADIAN = Math.PI / 180;
-        const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
-        const sin = Math.sin(-RADIAN * midAngle);
-        const cos = Math.cos(-RADIAN * midAngle);
-        const sx = cx + (outerRadius + 10) * cos;
-        const sy = cy + (outerRadius + 10) * sin;
-        const mx = cx + (outerRadius + 30) * cos;
-        const my = cy + (outerRadius + 30) * sin;
-        const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-        const ey = my;
-        const textAnchor = cos >= 0 ? 'start' : 'end';
-        console.log(props)
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
         return (
-            <g>
-                <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
-                    {payload.name}
-                </text>
-                <Sector
-                    cx={cx}
-                    cy={cy}
-                    innerRadius={innerRadius}
-                    outerRadius={outerRadius}
-                    startAngle={startAngle}
-                    endAngle={endAngle}
-                    fill={fill}
-                />
-                <Sector
-                    cx={cx}
-                    cy={cy}
-                    startAngle={startAngle}
-                    endAngle={endAngle}
-                    innerRadius={outerRadius + 6}
-                    outerRadius={outerRadius + 10}
-                    fill={fill}
-                />
-                <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-                <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-                <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`완료 ${value}`}</text>
-                <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-                    {`(Rate ${(percent * 100).toFixed(2)}%)`}
-                </text>
-            </g>
+            <text x={x} y={y} fill="black" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`예약 : ${value}건`}
+            </text>
         );
     };
+
 
     class TimeRound extends PureComponent {
 
@@ -179,16 +147,16 @@ function Home(props) {
         };
         render() {
             return (
-                <ResponsiveContainer width="90%" height={300}>
+                <ResponsiveContainer width="90%" height={500}>
                 <PieChart>
                     <Pie
-                        activeIndex={this.state.activeIndex}
-                        activeShape={renderActiveShape}
+                        labelLine={false}
+                        label={renderCustomizedLabel}
                         data={data}
                         cx="50%"
                         cy="50%"
-                        innerRadius="10%"
-                        outerRadius="40%"
+                        innerRadius="30%"
+                        outerRadius="90%"
                         fill="#8884d8"
                         dataKey="완료"
                         onMouseEnter={this.onPieEnter}
