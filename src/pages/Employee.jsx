26f571dbc623ce8e8361fad72b2e9eb383fa3c23
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-04-23 18:11:14 +0900

    no message

--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import locale from "antd/es/date-picker/locale/ko_KR";
-import {Breadcrumb} from "antd";
+import {Breadcrumb, Select, Input, Button} from "antd";
 import {useNavigate} from "react-router-dom";
-
+import styles from "../css/employee.module.css"
+import {RedoOutlined, SearchOutlined} from "@ant-design/icons";
 
 dayjs.extend(customParseFormat);
 
 function Employee(props) {
     const emplNavi = useNavigate();
-
+    const {Option} = Select;
+    const division = [{id:"all",name:"전체"},{id:1,name:"계약직"},{id:2,name:"정규직"},{id:3,name:"관리자"}];
     return (
         <>
             <div className="content">
                 <div>
-                    <div>
                         <Breadcrumb
                             separator=">"
                             items={[
@@ -33,8 +34,42 @@ function Employee(props) {
 
                             ]}
                         />
-                    </div>
                 </div>
+                <div>
+                    <div className={styles.searchCondition}>
+                    <Select
+                        placeholder="카테고리 선택"
+                        defaultValue="all"
+                        style={{width: '150px'}}
+                        allowClear={false}>
+                        {division.map((el) => (
+                            <Option key={el.id} value={el.id}>
+                                {el.name}
+                            </Option>
+                        ))}
+                    </Select>
+                    <Input
+                        placeholder="이름 검색"
+                        style={{width: '200px'}}
+                        
+                            />
+                        <Button
+                            icon={<SearchOutlined/>}
+                            // onClick={handleSearch}
+                        >
+                            조회
+                        </Button>
+                        <Button
+                            icon={<RedoOutlined/>}
+                            // onClick={handleReset}
+                        >
+                            초기화
+                        </Button>
+                    </div>
+
+
+                </div>
+
             </div>
         </>
     );
