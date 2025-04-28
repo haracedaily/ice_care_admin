ed20c11b7f41890cf6bd6692ac633681349b22bd
DESKTOP-29VI7GO\user <ssssssy0@naver.com>
2025-04-28 16:54:46 +0900

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,51 @@ import Body from "./layout/Body.jsx";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import {MenuUnfoldOutlined} from "@ant-design/icons";
 import {useNavigate} from "react-router-dom";
-function App() {
-const[toggleAside,setToggleAside]=useState(true);
-const [login,setLogin]=useState("");
-let navigator = useNavigate();
-useEffect(() => {
-    window.scrollTo({top: 0, left: 0});
-    if(localStorage.getItem("log"))setLogin(JSON.parse(localStorage.getItem("log")));
-    else if(!login)navigator("/login");
-},[location.pathname]);
-const logout = () => {
-    localStorage.clear();
-    setLogin("");
-    navigator("/login");
-}
-  return (
-    <>
-        {login?
-            <>
-            <Side toggleAside={toggleAside} setToggleAside={setToggleAside} login={login} />
-            <main className="login">
-                <nav>
-                    <section>
-                        <MenuUnfoldOutlined style={{ fontSize: '32px' }} onClick={()=>{setToggleAside(!toggleAside)}}/>
-                        <AccountCircleIcon style={{fontSize:'50px'}} onClick={logout}/>
-                    </section>
-                </nav>
-                <div>
-                    <Body/>
-                </div>
-            </main></>:<div>
-            <Body setLogin={setLogin} />
-            </div>}
 
-    </>
-  )
+function App() {
+    const [toggleAside, setToggleAside] = useState(true);
+    const [login, setLogin] = useState("");
+    let navigator = useNavigate();
+
+    useEffect(() => {
+        window.scrollTo({top: 0, left: 0});
+
+        if (localStorage.getItem("log") && typeof localStorage.getItem("log") === "object") {
+            setLogin(JSON.parse(localStorage.getItem("log")));
+        }
+        else if (!login) navigator("/login");
+
+    }, [location.pathname]);
+
+    const logout = () => {
+        localStorage.clear();
+        setLogin("");
+        navigator("/login");
+    }
+    return (
+        <>
+            {login ?
+                <>
+                    <Side toggleAside={toggleAside} setToggleAside={setToggleAside} login={login}/>
+                    <main className="login">
+                        <nav>
+                            <section>
+                                <MenuUnfoldOutlined style={{fontSize: '32px'}} onClick={() => {
+                                    setToggleAside(!toggleAside)
+                                }}/>
+                                <AccountCircleIcon style={{fontSize: '50px'}} onClick={logout}/>
+                            </section>
+                        </nav>
+                        <div>
+                            <Body/>
+                        </div>
+                    </main>
+                </> : <div>
+                    <Body setLogin={setLogin}/>
+                </div>}
+
+        </>
+    )
 }
 
 export default App
