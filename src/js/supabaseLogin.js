32680a4db2c0198e7c578624567c759a869ada69
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-06-17 17:23:54 +0900

    no message

--- src/js/supabaseLogin.js
+++ src/js/supabaseLogin.js
@@ -1,8 +1,17 @@
 import {supabase} from "./supabase.js";
 import {notification} from "antd";
 import bcrypt from "bcryptjs";
+import axios from "axios";
+const reqLogin = async(id) => {
+    axios.defaults.withCredentials = true;// 쿠키를 포함하여 요청을 보냄
+    const result = await axios.post('http://localhost:4002/back/login',{id})
+    console.log('return 값 = ',result);
+    return result;
+};
 
 export const getLogin = async(id,pw)=>{
+    const result = await reqLogin(id);
+    console.log("노드와 연결된 데이터 조회 값 = ",result);
     let res = await supabase.from("member").select("*").eq("id",id).single();
     if(res.error){
         notification.error({message:"에러발생",description:`${res.error.code==='PGRST116'?'존재하지 않는 아이디입니다.':res.error.details}`});
